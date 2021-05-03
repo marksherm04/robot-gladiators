32cb99ddbc5711d9e8d6a66ec310b2300c5cf1a3
Mark Sherman <marksherman@marks-imac.lan>
2021-05-02 23:28:11 -0400

    updated feature/shop branch

--- assets/js/game.js
+++ assets/js/game.js
@@ -140,7 +140,7 @@ var shop = function() {
 
   // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case 'REFILL':
+    case 'REFILL': // new case
     case 'refill':
       if (playerMoney >= 7) {
         window.alert("Refilling player's health by 20 for 7 dollars.");
@@ -153,7 +153,7 @@ var shop = function() {
         window.alert("You don't have enough money!");
     }
       break;
-    case 'UPGRADE':
+    case 'UPGRADE': // new case
     case 'upgrade':
       if (playerMoney >= 7) {
         window.alert("Upgrading player's attack by 6 for 7 dollars.");
@@ -166,7 +166,7 @@ var shop = function() {
         window.alert("You don't have enough money!");
     }
       break;
-    case 'LEAVE':
+    case 'LEAVE': // new case
     case 'leave':
       window.alert('Leaving the store.');
 
